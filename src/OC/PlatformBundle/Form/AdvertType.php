1ae675811a7450ec9c662016d7a192e2032afd57
ralexandre75 <rene.alexandre@grimoire-design.fr>
2018-04-09 21:29:18 +0200

    creation du constructeur de formulaire

---
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace OC\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdvertType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', 'datetime')
+            ->add('title')
+            ->add('author')
+            ->add('content')
+            ->add('published')
+            ->add('updatedAt', 'datetime')
+            ->add('nbApplications')
+            ->add('slug')
+            ->add('image')
+            ->add('categories')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OC\PlatformBundle\Entity\Advert'
+        ));
+    }
+}
