4c5b9a77d39f956304ee90d81f4c403023b68f72
ralexandre75 <rene.alexandre@grimoire-design.fr>
2018-04-10 23:05:21 +0200

    mise en place formulaire category

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -26,6 +27,19 @@ class AdvertType extends AbstractType
             ->add('content',        TextareaType::class)
             ->add('published',      CheckboxType::class, array('required' => false))
             ->add('image',          ImageType::class)
+
+      /*
+       * Rappel :
+       ** - 1er argument : nom du champ, ici « categories », car c'est le nom de l'attribut
+       ** - 2e argument : type du champ, ici « CollectionType » qui est une liste de quelque chose
+       ** - 3e argument : tableau d'options du champ
+       */
+      ->add('categories', CollectionType::class, array(
+        'entry_type'   => CategoryType::class,
+        'allow_add'    => true,
+        'allow_delete' => true
+      ))
+
             ->add('save',           SubmitType::class)
         ;
     }
