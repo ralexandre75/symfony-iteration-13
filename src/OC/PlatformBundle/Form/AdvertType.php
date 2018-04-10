9fdbca330679c2179aade44ed15431ca052515bc
ralexandre75 <rene.alexandre@grimoire-design.fr>
2018-04-10 19:12:22 +0200

    mise en place du formulaire dans AdvertType et lien dans le constructor

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -6,25 +6,32 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextereaType;
-use Symfony\Component\Form\Extension\Core\Type\Textype;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdvertType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date',       DateTimeType::class)
-            ->add('title',      Textype::class)
-            ->add('author',     Textype::class)
-            ->add('content',    TextareaType::class)
-            ->add('published',  CheckboxType::class, array('required' => false))
-            ->add('save',       SubmitType::class)
-            ;
+            ->add('date',           DateTimeType::class)
+            ->add('title',          TextType::class)
+            ->add('author',         TextType::class)
+            ->add('content',        TextareaType::class)
+            ->add('published',      CheckboxType::class, array('required' => false))
+            ->add('save',           SubmitType::class)
+        ;
     }
     
+    /**
+     * @param OptionsResolver $resolver
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
