f2c981faf460c5d3ebcb8573c83be7a3ace4fa77
ralexandre75 <rene.alexandre@grimoire-design.fr>
2018-04-13 23:04:59 +0200

    Heritage de formulaire

--- src/OC/PlatformBundle/Form/AdvertType.php
+++ src/OC/PlatformBundle/Form/AdvertType.php
@@ -12,6 +12,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdvertType extends AbstractType
@@ -21,13 +23,17 @@ class AdvertType extends AbstractType
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    {   
+        // Arbitrairement, on récupère toutes les catégories qui commencent par "D"
+
+        $pattern = 'D%';
+
         $builder
             ->add('date',           DateTimeType::class)
             ->add('title',          TextType::class)
             ->add('author',         TextType::class)
             ->add('content',        TextareaType::class)
-            ->add('published',      CheckboxType::class, array('required' => false))
+        //  ->add('published',      CheckboxType::class, array('required' => false))
             ->add('image',          ImageType::class)
 
       /*
@@ -43,15 +49,39 @@ class AdvertType extends AbstractType
       // ))
 
       
-      ->add('categories', EntityType::class, array(
-        'class'        => 'OCPlatformBundle:Category',
-        'choice_label' => 'name',
-        'multiple'     => true,
+            ->add('categories', EntityType::class, array(
+              'class'        => 'OCPlatformBundle:Category',
+              'choice_label' => 'name',
+              'multiple'     => true,
+              'query_builder'=> function(CategoryRepository $repository) use($pattern){
+                return $repository->getLikeQueryBuilder($pattern);
+        }
       ))
 
 
             ->add('save',           SubmitType::class)
         ;
+        // On ajoute une fonction qui va écouter un évènement
+        $builder->addEventListener(
+          FormEvents::PRE_SET_DATA, // 1er argument : L'événement qui nous intéresse : ici, PRE_SET_DATA
+          function(FormEvent $event) { // 2e argument : La fonction à exécuter lorsque l'événement est déclencher
+          // On récupère notre objet Advert sous-jacent
+          $advert = $event->getData();
+
+          // Cette condition est importante, on en reparle plus loin
+          if(null === $advert){
+            return; // On sort de la fonction sans rien faire lorsque $advert vaut null
+          }
+          // Si l'annonce n'est pas publiée, ou si elle n'existe pas encore en base (id est null)
+          if(!$advert->getPublished() || null === $advert->getId()) {
+            // Alors on ajoute le champ published
+            $event->getForm()->add('published', CheckboxType::class,array('required' => false));
+          } else
+            // Sinon, on le supprime
+            $event->getForm()->remove('published');
+          }
+        
+      );
     }
     
     /**
